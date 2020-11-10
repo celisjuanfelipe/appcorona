b1035627ab2c32fd97cab05b104e2cb8c796f981
celisjuanfelipe <juuan.felipe@gmail.com>
2020-11-09 23:50:55 -0300

    se agrega formulario de ingreso de datos (region)

--- src/routes/casos.js
+++ src/routes/casos.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/casos/add', (req, res) => {
+    res.render('casos/nc-region');
+});
+
 router.get('/casos', (req, res) =>{
     res.send('cases from database');
 
